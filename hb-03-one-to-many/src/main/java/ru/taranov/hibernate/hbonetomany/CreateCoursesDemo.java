b78d35a4913085e5ed4f8abc4588ee747fd17805
alexey-taranov <penspinner97@yandex.ru>
2019-12-31 18:27:13 +0300

    add one-to-many.

---
+++ hb-03-one-to-many/src/main/java/ru/taranov/hibernate/hbonetomany/CreateCoursesDemo.java
@@ -0,0 +1,50 @@
+package ru.taranov.hibernate.hbonetomany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.taranov.hibernate.hbonetomany.entity.Course;
+import ru.taranov.hibernate.hbonetomany.entity.Instructor;
+import ru.taranov.hibernate.hbonetomany.entity.InstructorDetail;
+
+public class CreateCoursesDemo {
+
+    public static void main(String[] args) {
+        //create session factory
+        SessionFactory sessionFactory = new Configuration()
+                .configure()
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        //create session
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            // start a transaction
+            session.beginTransaction();
+
+            int theId = 3;
+            Instructor tempInstructor = session.get(Instructor.class, theId);
+
+            // create some courses
+            Course tempCourse1 = new Course("Air Guitar - The Ultimate Guide");
+            Course tempCourse2 = new Course("The Pinball Masterclass");
+
+            // add courses to instructor
+            tempInstructor.add(tempCourse1);
+            tempInstructor.add(tempCourse2);
+
+            // save the courses
+            session.save(tempCourse1);
+            session.save(tempCourse2);
+
+            // commit transaction
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+}
---
+++ hb-03-one-to-many/src/main/java/ru/taranov/hibernate/hbonetomany/DeleteInstructorDetailDemo.java
@@ -0,0 +1,44 @@
+package ru.taranov.hibernate.hbonetomany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.taranov.hibernate.hbonetomany.entity.Instructor;
+import ru.taranov.hibernate.hbonetomany.entity.InstructorDetail;
+
+public class DeleteInstructorDetailDemo {
+
+    public static void main(String[] args) {
+        //create session factory
+        SessionFactory sessionFactory = new Configuration()
+                .configure()
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        //create session
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            int theId = 2;
+
+            InstructorDetail instructorDetail = session.get(InstructorDetail.class, theId);
+
+            System.out.println("the associated instructor: " + instructorDetail.getInstructor());
+
+            // now let's delete the instructor detail
+            instructorDetail.getInstructor().setInstructorDetail(null);
+            session.delete(instructorDetail);
+
+            // commit transaction
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+}
